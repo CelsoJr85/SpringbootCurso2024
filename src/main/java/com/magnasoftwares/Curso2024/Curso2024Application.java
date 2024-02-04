06f6a6fd1b7deac5e16ec17631d01b7a8a28d8b1
CelsoJr85 <celsocustodiojunior1985@gmail.com>
2024-02-03 21:56:46 -0300

    ProjetoSpringboot

---
+++ src/main/java/com/magnasoftwares/Curso2024/Curso2024Application.java
@@ -0,0 +1,14 @@
+package com.magnasoftwares.Curso2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curso2024Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Curso2024Application.class, args);
+	}
+
+}
